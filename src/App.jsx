6aab785518f4075a956896bf9e930b874f62a7e5
Emilse Arias <emilsearias@gmail.com>
2023-05-15 18:44:17 -0300

    agregamos el componente Titulos el cual recibe un props e instalamos react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
+import "./App.css";
+import Titulos from "./components/Titulos";
 
 function App() {
- //aqui es donde podemos escribir logica
+  //aqui es donde podemos escribir logica
 
   return (
-   <div></div>
-  )
+    // aqui puedo escribir un poquito de logica
+    //<> es un Fragmento
+    <>
+      {/* aqui va el maquetado del componente */}
+      {/* <Titulos aqui va los props></Titulos> */}
+      <Titulos nuevoTitulo='Este titulo fue creado con props' estado={true} />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Titulos.jsx
@@ -0,0 +1,12 @@
+const Titulos = ({nuevoTitulo}) => {
+    // console.log(props.nuevoTitulo)
+
+  return (
+    <section>
+      <h1>Primer App react</h1>
+      <h2>{nuevoTitulo}</h2>
+    </section>
+  );
+};
+
+export default Titulos;
